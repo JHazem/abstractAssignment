ff23618dcaf7fe4eccee774bb4e0f2db5bc92e3b
Hazem <junbsher@gmail.com>
2022-12-25 00:24:35 -0600

    Organizing files

---
+++ src/com/perscholas/HackerRankArrayList.java
@@ -0,0 +1,90 @@
+package com.perscholas;
+
+public class HackerRankArrayList {
+
+	/* https://www.hackerrank.com/challenges/java-arraylist/problem
+	   
+	 Sometimes it's better to use dynamic size arrays. 
+	 Java's Arraylist can provide you this feature. 
+	 Try to solve this problem using Arraylist.
+
+	You are given  lines. In each line there are zero or more integers. 
+	You need to answer a few queries where you need to tell the number located in  
+	position of  line.
+	
+	Take your input from System.in.
+	
+	Input Format
+	The first line has an integer . In each of the next  lines there will be an integer  
+	denoting number of integers on that line and then there will be  space-separated 
+	integers. In the next line there will be an integer  denoting number of queries. 
+	Each query will consist of two integers  and .
+	
+	Constraints
+	
+	Each number will fit in signed integer.
+	Total number of integers in  lines will not cross .
+	
+	Output Format
+	In each line, output the number located in  position of  line. 
+	If there is no such position, just print "ERROR!"
+
+
+	 */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/* https://www.hackerrank.com/challenges/java-iterator/problem
+	 
+	 Java Iterator class can help you to iterate through every element in a collection. 
+	 Here is a simple example:
+		
+		import java.util.*;
+		public class Example{
+		
+		    public static void main(String []args){
+		        ArrayList mylist = new ArrayList();
+		        mylist.add("Hello");
+		        mylist.add("Java");
+		        mylist.add("4");
+		        Iterator it = mylist.iterator();
+		        while(it.hasNext()){
+		            Object element = it.next();
+		            System.out.println((String)element);
+		        }
+		    }
+		}
+		In this problem you need to complete a method func. 
+		The method takes an ArrayList as input. 
+		In that ArrayList there is one or more integer numbers, 
+		then there is a special string "###", 
+		after that there are one or more other strings.
+		 A sample ArrayList may look like this:
+		
+		element[0]=>42
+		element[1]=>10
+		element[2]=>"###"
+		element[3]=>"Hello"
+		element[4]=>"Java"
+		You have to modify the func method by editing at most 2 lines so that the code only prints the elements after the special string "###". For the sample above the output will be:
+		
+		Hello
+		Java
+		Note: The stdin doesn't contain the string "###", it is added in the main method.
+		
+		To restore the original code in the editor, 
+		click the top left icon on the editor and create a new buffer.
+	 */
+}
